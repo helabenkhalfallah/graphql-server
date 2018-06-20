caf0e6626c158b6d6842c444cd90604220105f58
helabenkhalfallah <hela@upperskills.com>
2018-06-20 15:30:39 +0100

    [graphql-server] Photo Queries and Mutations

--- grapql/photos/mutations/PhotoAdd.js
+++ grapql/photos/mutations/PhotoAdd.js
@@ -1,7 +1,6 @@
 import { GraphQLNonNull, GraphQLString } from 'graphql'
 import AppModels from '../../../models/index'
 import Photo from '../types/Photo'
-import AppLogger from '../../../core/logger/AppLogger';
 
 //add
 let PhotoAdd = {
@@ -24,14 +23,10 @@ let PhotoAdd = {
     return new Promise((resolve, reject) => {
       AppModels.PhotoModel.findOne({ path: params.path }, (error, photo) => {
         // insert only if photo path not exist
-        AppLogger.debug('PhotoAdd photo - ', photo)
-        AppLogger.debug('PhotoAdd error - ', error)
         if (!error) {
           if (!photo) {
             const photoModel = new AppModels.PhotoModel(params)
-            AppLogger.debug('PhotoAdd photoModel - ', photoModel)
             let newPhoto = photoModel.save(photoModel)
-            AppLogger.debug('PhotoAdd newPhoto - ', newPhoto)
             if (newPhoto) {
               resolve(newPhoto)
             } else {
